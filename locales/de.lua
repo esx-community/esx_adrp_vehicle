13345f4e66cf4d646e63a87f0ed4f885609b41f3
MintKind <Luca@steinhorst.me>
2020-03-04 20:16:01 +0100

    Created de.lua

---
+++ locales/de.lua
@@ -0,0 +1,37 @@
+Locales['de'] = {
+  -- Trunk
+  ['trunk_locked'] = 'Der Kofferraum ist abgeschlossen',
+  ['trunk_title'] = 'Kofferraum - Hauptmenü',
+  ['trunk_busy'] = 'Der Kofferraum wird bereits von jemand anderem benutzt',
+  ['trunk_take'] = 'Aus Kofferraum nehmen',
+  ['trunk_take_title'] = 'Kofferraum - Nehmen von Fahrzeug [%s/%s]',
+  ['trunk_take_quantity'] = 'Betrag, der aus dem Kofferraum entnommen werden soll',
+  ['trunk_deposit'] = 'In Kofferraum lagern',
+  ['trunk_deposit_title'] = 'Kofferraum - Lagern in Fahrzeug [%s/%s]',
+  ['trunk_deposit_quantity'] = 'Anzahl aus deinem Inventar',
+
+  -- Errors
+  ['error_remove_hasweapon'] = 'Du hast diese Waffe bereits!',
+  ['error_remove_enoughblack'] = 'Es ist nicht genung ~r~Schwarzgeld~s~ im Kofferraum',
+  ['error_remove_playerlimit'] = 'Du kannst all das nicht tragen ~b~%s~s~',
+  ['error_remove_trunk'] = 'Es ist nicht genung ~b~%s~s~ im Kofferraum!',
+  ['error_add_playerenough'] = 'Du hast nicht genug ~b~%s~s~',
+  ['error_add_weaponalready'] = 'Du kannst nur einmal die gleiche Waffe einlagern',
+  ['error_add_enoughblack'] =  'Es ist nicht genung ~r~Schwarzgeld~s~ in deinem Inventar',
+  ['error_add_weaponmissing'] = 'Waffe wurde nicht in deinem Inventar gefunden! Exploit versuch wurde geloggt!',
+  ['error_add_vehiclelimit'] = 'Der Kofferraum ist voll und kann den Gegenstand nicht mehr aufnehmen!',
+  ['error_left_menu'] = 'Es sieht so aus als wärst du dem Kofferraum entkommen!',
+
+  -- Key Chain
+  ['keychain_title'] = 'Schlüsselbung - Hauptmenü',
+  ['keychain_actions_title'] = 'Schlüsselbung - Aktionen',
+  ['keychain_givekey'] = 'Schlüssel geben',
+  ['keychain_dropkey'] = 'Schlüssel wegwerfen',
+  ['keychain_dropped'] = 'Du hast den Schlüssel weggeworfen!',
+  ['keychain_give_title'] = 'Schlüsselbung - Schlüssel geben',
+  ['keychain_give_nonearby'] = 'Es ist niemand in der nähe!',
+  ['keychain_give_playerlost'] = 'Spieler wurde nicht gefunden oder ist zu weit weg!',
+  ['keychain_given'] = 'Du hast die Schlüssel zu dem Fahrzeug mit dem Kennzeichen ~y~%s~s~ abgegeben',
+  ['keychain_given_target'] = '~b~%s~s~ hat dir die Schlüssel zu dem Fahrzeug mit dem Kennzeichen ~y~%s~s~ gegeben',
+  ['keychain_notgiven'] = 'Schlüssel konnten nicht weitergegeben werden. Stellen Sie sicher, dass der Spieler sie noch nicht hat',
+}
