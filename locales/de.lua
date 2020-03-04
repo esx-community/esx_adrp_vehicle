13fe40aefb6fc69b9c52ee64f7ac1f03042500e6
rex2630 <rex2630@gmail.com>
2020-03-04 20:38:52 +0100

    Added german translation (#2)
    * Created de.lua
    
    * Update fxmanifest.lua
    
    * Updated de.lua
    
    Changed translationes to start with lowercase letters

---
+++ locales/de.lua
@@ -0,0 +1,37 @@
+Locales['de'] = {
+  -- Trunk
+  ['trunk_locked'] = 'der Kofferraum ist abgeschlossen',
+  ['trunk_title'] = 'kofferraum - Hauptmenü',
+  ['trunk_busy'] = 'der Kofferraum wird bereits von jemand anderem benutzt',
+  ['trunk_take'] = 'aus Kofferraum nehmen',
+  ['trunk_take_title'] = 'kofferraum - Nehmen von Fahrzeug [%s/%s]',
+  ['trunk_take_quantity'] = 'betrag, der aus dem Kofferraum entnommen werden soll',
+  ['trunk_deposit'] = 'in Kofferraum lagern',
+  ['trunk_deposit_title'] = 'kofferraum - Lagern in Fahrzeug [%s/%s]',
+  ['trunk_deposit_quantity'] = 'anzahl aus deinem Inventar',
+
+  -- Errors
+  ['error_remove_hasweapon'] = 'du hast diese Waffe bereits!',
+  ['error_remove_enoughblack'] = 'es ist nicht genung ~r~Schwarzgeld~s~ im Kofferraum',
+  ['error_remove_playerlimit'] = 'du kannst all das nicht tragen ~b~%s~s~',
+  ['error_remove_trunk'] = 'es ist nicht genung ~b~%s~s~ im Kofferraum!',
+  ['error_add_playerenough'] = 'du hast nicht genug ~b~%s~s~',
+  ['error_add_weaponalready'] = 'du kannst nur einmal die gleiche Waffe einlagern',
+  ['error_add_enoughblack'] =  'es ist nicht genung ~r~Schwarzgeld~s~ in deinem Inventar',
+  ['error_add_weaponmissing'] = 'waffe wurde nicht in deinem Inventar gefunden! Exploit versuch wurde geloggt!',
+  ['error_add_vehiclelimit'] = 'der Kofferraum ist voll und kann den Gegenstand nicht mehr aufnehmen!',
+  ['error_left_menu'] = 'es sieht so aus als wärst du dem Kofferraum entkommen!',
+
+  -- Key Chain
+  ['keychain_title'] = 'schlüsselbung - Hauptmenü',
+  ['keychain_actions_title'] = 'schlüsselbung - Aktionen',
+  ['keychain_givekey'] = 'schlüssel geben',
+  ['keychain_dropkey'] = 'schlüssel wegwerfen',
+  ['keychain_dropped'] = 'du hast den Schlüssel weggeworfen!',
+  ['keychain_give_title'] = 'schlüsselbung - Schlüssel geben',
+  ['keychain_give_nonearby'] = 'es ist niemand in der nähe!',
+  ['keychain_give_playerlost'] = 'spieler wurde nicht gefunden oder ist zu weit weg!',
+  ['keychain_given'] = 'du hast die Schlüssel zu dem Fahrzeug mit dem Kennzeichen ~y~%s~s~ abgegeben',
+  ['keychain_given_target'] = '~b~%s~s~ hat dir die Schlüssel zu dem Fahrzeug mit dem Kennzeichen ~y~%s~s~ gegeben',
+  ['keychain_notgiven'] = 'schlüssel konnten nicht weitergegeben werden. Stellen Sie sicher, dass der Spieler sie noch nicht hat',
+}
