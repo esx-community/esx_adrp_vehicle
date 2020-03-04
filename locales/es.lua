cefae7aa7251245a9fa72a4619165627530433b0
SEOAlexRamon <37023161+seoalexramon@users.noreply.github.com>
2020-03-04 12:48:43 +0100

    Spanish language added

---
+++ locales/es.lua
@@ -0,0 +1,37 @@
+Locales['es'] = {
+  -- Maletero
+  ['trunk_locked'] = 'el maletero está bloqueado',
+  ['trunk_title'] = 'maletero - Menu principal',
+  ['trunk_busy'] = 'el maletero está actualmente en uso, alguien lo estará usando',
+  ['trunk_take'] = 'retirar del vehiculo',
+  ['trunk_take_title'] = 'maletero - Retirar del vehiculo [%s/%s]',
+  ['trunk_take_quantity'] = 'cantidad a retirar del maletero',
+  ['trunk_deposit'] = 'guardar en el vehiculo',
+  ['trunk_deposit_title'] = 'maletero - Guardar en el vehiculo [%s/%s]',
+  ['trunk_deposit_quantity'] = 'cantidad a depositar del inventario',
+
+  -- Errores
+  ['error_remove_hasweapon'] = 'ya dispones de la misma arma en tu inventario!',
+  ['error_remove_enoughblack'] = 'no hay suficiente ~r~Dinero Negro~s~ en el maletero',
+  ['error_remove_playerlimit'] = 'no puedes cargar con todo eso ~b~%s~s~',
+  ['error_remove_trunk'] = 'no hay suficiente ~b~%s~s~ en el maletero!',
+  ['error_add_playerenough'] = 'no tienes suficiente ~b~%s~s~',
+  ['error_add_weaponalready'] = 'no puedes guardar mas de una arma del mismo modelo',
+  ['error_add_enoughblack'] =  'no hay suficiente ~r~Dinero Negro~s~ en tu inventario',
+  ['error_add_weaponmissing'] = 'no se ha encontrado el arma en tu inventario. Intento de aprovechar un possible fallo detectado',
+  ['error_add_vehiclelimit'] = 'el inventario del vehiculo está en su máxima capacidad!',
+  ['error_left_menu'] = 'parece que has escapado de alguna forma del maletero!',
+
+  -- Llavero
+  ['keychain_title'] = 'Llavero - Menu principal',
+  ['keychain_actions_title'] = 'Llavero - Acciones',
+  ['keychain_givekey'] = 'Dar llave',
+  ['keychain_dropkey'] = 'Tirar llave',
+  ['keychain_dropped'] = 'Has tirado una llave!',
+  ['keychain_give_title'] = 'Llavero - Dar llave',
+  ['keychain_give_nonearby'] = 'no hay nadie cerca de ti!',
+  ['keychain_give_playerlost'] = 'no se ha encontrado el jugador o esta demasiado lejos de ti',
+  ['keychain_given'] = 'le has dado las llaves del vehiculo con matrícula ~y~%s~s~',
+  ['keychain_given_target'] = '~b~%s~s~ te dió las llaves del vehiculo con matrícula ~y~%s~s~',
+  ['keychain_notgiven'] = 'no se pudo dar las llaves. Asegurate de que no las tenga ya.',
+}
