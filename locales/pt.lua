e1692527e8acf611d104bd0685747d9b8ee0d21b
rex2630 <rex2630@gmail.com>
2020-03-05 07:04:15 +0100

    Added european portuguese translation (#3)

---
+++ locales/pt.lua
@@ -0,0 +1,37 @@
+Locales['pt'] = {
+  -- Trunk
+  ['trunk_locked'] = 'o porta-bagagens está fechado',
+  ['trunk_title'] = 'porta-bagagens - Menu principal',
+  ['trunk_busy'] = 'o porta-bagagens está atualmente ocupado, outra pessoa deve estar a usa-lo',
+  ['trunk_take'] = 'retirar do veículo',
+  ['trunk_take_title'] = 'porta-bagagens - Retirar do veículo [%s/%s]',
+  ['trunk_take_quantity'] = 'quantidade a retirar do porta-bagagens',
+  ['trunk_deposit'] = 'guarde no veículo',
+  ['trunk_deposit_title'] = 'porta-bagagens - Guardar no veículo [%s/%s]',
+  ['trunk_deposit_quantity'] = 'quantidade a depositar do teu inventário',
+
+  -- Errors
+  ['error_remove_hasweapon'] = 'já tens uma arma igual contigo!',
+  ['error_remove_enoughblack'] = 'não existe quantidade suficiente de ~r~Dinheiro Sujo~s~ no porta-bagagens',
+  ['error_remove_playerlimit'] = 'não consegue carregar toda essa quantidade de ~b~%s~s~',
+  ['error_remove_trunk'] = 'não existe quantidade suficiente de ~b~%s~s~ no porta-bagagens!',
+  ['error_add_playerenough'] = 'não tens quantidade suficiente de ~b~%s~s~',
+  ['error_add_weaponalready'] = 'não consegues guardar mais do que uma arma do mesmo modelo',
+  ['error_add_enoughblack'] =  'não existe quantidade suficiente de ~r~Dinheiro Sujo~s~ no teu inventário',
+  ['error_add_weaponmissing'] = 'não foi possível encontrar a arma no teu inventário. Tentativa de tirar proveito de uma possível falha detectada',
+  ['error_add_vehiclelimit'] = 'o porta-bagagens está cheio, portanto não consegue guardar isso!',
+  ['error_left_menu'] = 'parece que de alguma forma escapou do porta-malas!',
+
+  -- Key Chain
+  ['keychain_title'] = 'porta-chaves - Menu principal',
+  ['keychain_actions_title'] = 'porta-chaves - Ações',
+  ['keychain_givekey'] = 'dar chave',
+  ['keychain_dropkey'] = 'largar chave',
+  ['keychain_dropped'] = 'largaste uma chave!',
+  ['keychain_give_title'] = 'porta-chaves - Dar chave',
+  ['keychain_give_nonearby'] = 'não está ninguém perto de ti!',
+  ['keychain_give_playerlost'] = 'não foi possível encontrar o jogador ou então o mesmo está muito longe de ti',
+  ['keychain_given'] = 'deste as chaves do veículo com a matrícula ~y~%s~s~',
+  ['keychain_given_target'] = '~b~%s~s~ deu-te as chaves do veículo com a matrícula ~y~%s~s~',
+  ['keychain_notgiven'] = 'não foi possível dar as chaves. Verifique se o jogador já não as possui',
+}
